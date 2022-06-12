8bb4b6c73fc249840e693ad661011d838dd86047
yodadude2003 <yodadude2003@gmail.com>
2022-06-12 11:21:23 -0500

    Add CAP_DELAY flushing capabilities and fixed timestamp logic TODO (by someone someday): Handle HDR input for encoder.

--- libavcodec/libvvenc.c
+++ libavcodec/libvvenc.c
@@ -61,9 +61,9 @@ static av_cold int ff_vvenc_encode_init(AVCodecContext *avctx)
     vvenc_init_default (&q->params, avctx->width, avctx->height, avctx->framerate.num, avctx->bit_rate, avctx->global_quality, VVENC_FASTER);
 
     q->params.m_verbosity = VVENC_DETAILS;
-    if     ( av_log_get_level() >= AV_LOG_DEBUG )   q->params.m_verbosity = VVENC_DETAILS;
-    else if( av_log_get_level() >= AV_LOG_VERBOSE ) q->params.m_verbosity = VVENC_INFO;     // VVDEC_INFO will output per picture info
-    else if( av_log_get_level() >= AV_LOG_INFO )    q->params.m_verbosity = VVENC_WARNING;  // AV_LOG_INFO is ffmpeg default
+    if     (av_log_get_level() >= AV_LOG_DEBUG)   q->params.m_verbosity = VVENC_DETAILS;
+    else if(av_log_get_level() >= AV_LOG_VERBOSE) q->params.m_verbosity = VVENC_INFO;     // VVDEC_INFO will output per picture info
+    else if(av_log_get_level() >= AV_LOG_INFO)    q->params.m_verbosity = VVENC_WARNING;  // AV_LOG_INFO is ffmpeg default
     else q->params.m_verbosity = VVENC_SILENT;
 
     q->params.m_msgFnc = &ff_vvenc_log_callback;
@@ -71,15 +71,13 @@ static av_cold int ff_vvenc_encode_init(AVCodecContext *avctx)
     q->params.m_internChromaFormat = VVENC_CHROMA_420;
     if(avctx->pix_fmt == AV_PIX_FMT_YUV420P) {
         q->params.m_outputBitDepth[0] = q->params.m_internalBitDepth[0] = q->params.m_inputBitDepth[0] = 8;
-    }
-    else if(avctx->pix_fmt == AV_PIX_FMT_YUV420P10LE) {
+    } else if(avctx->pix_fmt == AV_PIX_FMT_YUV420P10LE) {
         q->params.m_outputBitDepth[0] = q->params.m_internalBitDepth[0] = q->params.m_inputBitDepth[0] = 10;
     }
 
     if(avctx->thread_count > 0) {
         q->params.m_numThreads = avctx->thread_count;
-    }
-    else {
+    } else {
         q->params.m_numThreads = -1;
     }
 
@@ -107,39 +105,43 @@ static av_cold int ff_vvenc_encode_frame(AVCodecContext *avctx, AVPacket *avpkt,
     int pktSize;
     VVEnCContext *q = avctx->priv_data;
 
-    for(int i = 0; i < 3; i++) {
-        if(avctx->pix_fmt == AV_PIX_FMT_YUV420P10LE) {
-            memcpy(q->yuvbuf->planes[i].ptr, frame->data[i], q->yuvbuf->planes[i].height * frame->linesize[i]);
+    if(frame) {
+        for(int i = 0; i < 3; i++) {
+            if(avctx->pix_fmt == AV_PIX_FMT_YUV420P10LE) {
+                memcpy(q->yuvbuf->planes[i].ptr, frame->data[i], q->yuvbuf->planes[i].height * frame->linesize[i]);
+            } else if (avctx->pix_fmt == AV_PIX_FMT_YUV420P) {
+                ff_vvenc_expand_bytes(frame->data[i], q->yuvbuf->planes[i].ptr, q->yuvbuf->planes[i].height * frame->linesize[i]);
+            }
         }
-        else if (avctx->pix_fmt == AV_PIX_FMT_YUV420P) {
-            ff_vvenc_expand_bytes(frame->data[i], q->yuvbuf->planes[i].ptr, q->yuvbuf->planes[i].height * frame->linesize[i]);
+
+        q->yuvbuf->sequenceNumber = avctx->frame_number;
+
+        if(frame->pts != AV_NOPTS_VALUE) {
+            q->yuvbuf->cts      = av_rescale_q(frame->pts, avctx->time_base, av_make_q(1, q->params.m_TicksPerSecond));
+            q->yuvbuf->ctsValid = true;
         }
     }
 
-    q->yuvbuf->sequenceNumber = avctx->frame_number;
-    q->yuvbuf->cts            = av_rescale_q(frame->pts, av_make_q(1, q->params.m_TicksPerSecond), avctx->time_base);
-    q->yuvbuf->ctsValid       = true;
-
-    if(vvenc_encode(q->encoder, q->yuvbuf, q->au, &q->encDone)) {
+    if(vvenc_encode(q->encoder, frame ? q->yuvbuf : NULL, q->au, &q->encDone) < 0) {
         return -1;
     }
 
     pktSize = q->au->payloadUsedSize;
     if (pktSize > 0) {
-        ff_get_encode_buffer(avctx, avpkt, pktSize, 0);
+        ff_alloc_packet2(avctx, avpkt, pktSize, 0);
 
-        avpkt->dts = av_rescale_q(q->au->dts, av_make_q(1, q->params.m_TicksPerSecond), avctx->time_base);
-        avpkt->pts = av_rescale_q(q->au->cts, av_make_q(1, q->params.m_TicksPerSecond), avctx->time_base);
+        avpkt->dts = !q->au->dtsValid ? AV_NOPTS_VALUE : av_rescale_q(q->au->dts, av_make_q(1, q->params.m_TicksPerSecond), avctx->time_base);
+        avpkt->pts = !q->au->ctsValid ? AV_NOPTS_VALUE : av_rescale_q(q->au->cts, av_make_q(1, q->params.m_TicksPerSecond), avctx->time_base);
 
         if (q->au->refPic) {
-          avpkt->flags = AV_PKT_FLAG_KEY;
+            avpkt->flags = AV_PKT_FLAG_KEY;
         }
 
         memcpy(avpkt->data, q->au->payload, pktSize);
         *got_packet_ptr = 1;
     }
 
-    return 0;
+    return q->encDone ? AVERROR_EOF : 0;
 }
 
 static av_cold int ff_vvenc_encode_close(AVCodecContext *avctx)
@@ -154,8 +156,8 @@ static av_cold int ff_vvenc_encode_close(AVCodecContext *avctx)
 }
 
 AVCodec ff_libvvenc_encoder = {
-  	.name           = "libvvenc",
-		.long_name      = "H.266 / VVC Encoder VVenC",
+    .name           = "libvvenc",
+    .long_name      = "H.266 / VVC Encoder VVenC",
     .type           = AVMEDIA_TYPE_VIDEO,
     .id             = AV_CODEC_ID_VVC,
     .priv_data_size = sizeof(VVEnCContext),
@@ -165,7 +167,7 @@ AVCodec ff_libvvenc_encoder = {
     .pix_fmts       = (const enum AVPixelFormat[]){ AV_PIX_FMT_YUV420P,
                                                     AV_PIX_FMT_YUV420P10LE,
                                                     AV_PIX_FMT_NONE  },
-    .capabilities    = AV_CODEC_CAP_OTHER_THREADS,
-    .caps_internal   = FF_CODEC_CAP_AUTO_THREADS,
-    .wrapper_name    = "libvvenc",
+    .capabilities   = AV_CODEC_CAP_DELAY | AV_CODEC_CAP_OTHER_THREADS,
+    .caps_internal  = FF_CODEC_CAP_AUTO_THREADS | FF_CODEC_CAP_INIT_CLEANUP,
+    .wrapper_name   = "libvvenc",
 };
